9e90ecc653d6b39ba7e0fd645d2aedf878c3e5b8
sasagichuki@gmail.com <sasagichuki@gmail.com>
2013-04-03 11:02:51 +0300

    initial commit still trying to figure out TDD

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tasks::Application.config.secret_token = '2aa43c2f8486ebfe1b7fd28a8bfa8872da28df603fd74b27be020421b615f751c97d6e5c42f3afe6275f0fcb0626920ead44d09af4a590182e44f5ebe20b6f01'
